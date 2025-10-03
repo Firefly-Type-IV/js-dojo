d2f2a27b86c89daa83d3b8fa2fec4a2986b63f63
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-03 12:05:46 +0100

    #19 Using filter() method to 'filter' items from an array.

--- arrays/19.js
+++ arrays/19.js
@@ -8,4 +8,6 @@
 export function filterEvenAges(ages) {
   // TODO: Use the filter() method to get even numbers from the array
   // TODO: Return the new array of even ages
+  const evenAges = ages.filter(age => age % 2 === 0);
+  return evenAges;
 }
