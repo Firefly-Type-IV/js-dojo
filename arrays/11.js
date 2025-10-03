db53b8948a69dba9964d33841f50d7313f69ae47
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-03 11:21:31 +0100

    #11 Using map() method.

--- arrays/11.js
+++ arrays/11.js
@@ -8,4 +8,5 @@
 export function increasePrices(prices) {
   // TODO: Use the map() method to increase each price by 10%
   // TODO: Return the new array of increased prices
+  return prices.map(price => price * 1.1)
 }
