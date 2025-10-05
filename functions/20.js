bd835c26cea9d713b7a62e85d6bb00cd67cc3270
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-05 18:10:51 +0100

    12 functions conditions.

--- functions/20.js
+++ functions/20.js
@@ -10,4 +10,21 @@ export function ticketPrice(age, isMember) {
   // TODO: Determine the price based on the age and membership status
   // Prices: $8 for children (under 18), $10 for adults, $6 for seniors (65+)
   // Members get a $2 discount
+  // Initialise variable 'fee' as a number.
+  let fee = 0;
+  if (age >= 65){
+    fee = 6;
+    // Check highest age bracket first.
+  } else if (age < 18){
+    fee = 8;
+    // Child.
+  } else {
+    fee = 10;
+    // Remaining age group (18 to 64).
+  }
+  if (isMember){
+    return fee - 2;
+  }
+  return fee;
+  // Ternary operator applies discount if isMember is true, otherwise applies 0: const discount = isMember ? 2 : 0;
 }
