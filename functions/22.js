00226bfdeb63038b4cb245fdd112b8381a108a27
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-05 18:27:14 +0100

    22 functions logical AND operator.

--- functions/22.js
+++ functions/22.js
@@ -12,4 +12,15 @@ export function determineGrade(score) {
   // 70-79: 'C'
   // 60-69: 'D'
   // Below 60: 'F'
+  if (score >= 90){
+    return 'A';
+  } else if (score >= 80){
+    return 'B';
+  } else if (score >= 70){
+    return 'C';
+  } else if (score >= 60){
+    return 'D';
+  } else {
+    return 'F';
+  }
 }
